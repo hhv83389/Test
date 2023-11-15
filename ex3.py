aebc7d2d8f9cefe608667e52609a32115758ab33
hhv83389 <hhv83389@kwansei.ac.jp>
2023-11-15 18:11:11 +0900

    ex3

---
+++ ex3.py
@@ -0,0 +1,23 @@
+import zipfile  # zipfileモジュール
+
+InputZipFile = "sample.zip"  # 入力ファイル
+
+# 変数の初期化
+sum = 0  # 合計
+
+# ファイルの読み込み + 合計の計算
+# zipファイルの読み込み
+with zipfile.ZipFile(InputZipFile, "r") as zip_file:
+    InputFiles = zip_file.namelist()  # zipファイル内のファイルの配列
+    for InputFile in InputFiles:
+        if InputFile.endswith("_kgu.txt"):  # 「_kgu.txt」をファイル名の最後に持つファイル
+            file_number = int(InputFile.split("_")[1])  # ファイルの数値のみを抽出
+            if file_number % 2 == 1:  # 奇数名のみ
+                # print(file_number)
+                # txtファイルの読み込み
+                with zip_file.open(InputFile, "r") as f:
+                    number = f.read()  # ファイル内の数値
+                    sum += int(number)
+
+# 出力
+print(sum)
