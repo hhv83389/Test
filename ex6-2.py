bdb6f800b32cb279bc27d339cfc9c4b47dcdf443
hhv83389 <hhv83389@kwansei.ac.jp>
2023-11-29 18:37:33 +0900

    ex6-2

--- ex6-2.py
+++ ex6-2.py
@@ -2,8 +2,8 @@
 import zipfile
 import json
 
-# matplotlibモジュール
-import matplotlib.pyplot as plt
+# tkinterモジュール
+import tkinter as tk
 
 InputZipFile = "kabeposter.zip"  # 入力ファイル
 
@@ -17,21 +17,31 @@ with zipfile.ZipFile(InputZipFile, "r") as zip_file:
             with zip_file.open(InputFile, "r") as f:
                 data = json.load(f)
 
-# 散布図の作成
-plt.figure()
-plt.xlim(0, 2000)  # x軸
-plt.ylim(1500, 0)  # y軸
+# ウィンドウの作成
+root = tk.Tk()
+root.geometry("1200x800")  # サイズ
 
+# キャンバスの作成
+canvas = tk.Canvas(root, width=1200, height=800, bg="white")
+canvas.place(x=0, y=0)
+
+r = 5  # 半径
+t = 1 / 2  # 倍率
+
+# 2人の肩のラインの描画
+# 人
 for i in range(2):
     # 関節
-    # 配列には3つおきにX,Y,信頼度が格納されている。
-    X1 = data["people"][i]["pose_keypoints_2d"][2 * 3]
-    Y1 = data["people"][i]["pose_keypoints_2d"][2 * 3 + 1]
-    X2 = data["people"][i]["pose_keypoints_2d"][5 * 3]
-    Y2 = data["people"][i]["pose_keypoints_2d"][5 * 3 + 1]
-    # 各関節の座標をplot
-    plt.scatter(X1, Y1, c="black")
-    plt.scatter(X2, Y2, c="black")
+    X1 = data["people"][i]["pose_keypoints_2d"][2 * 3] * t
+    Y1 = data["people"][i]["pose_keypoints_2d"][2 * 3 + 1] * t
+    X2 = data["people"][i]["pose_keypoints_2d"][5 * 3] * t
+    Y2 = data["people"][i]["pose_keypoints_2d"][5 * 3 + 1] * t
 
-# 表示
-plt.show()
+    # 点の描画
+    canvas.create_oval(X1 - r, Y1 - r, X1 + r, Y1 + r, fill="black", width=0)
+    canvas.create_oval(X2 - r, Y2 - r, X2 + r, Y2 + r, fill="black", width=0)
+    canvas.create_line(X1, Y1, X2, Y2)
+
+
+# ウィンドウの表示
+root.mainloop()
